90ef8ffb3385ec5d168e02fe92943a5caef72faf
QuocThinh73 <ttqthinh2004@gmail.com>
2025-06-17 10:26:33 +0700

    add logout function into main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def login():
     print("Login")
 
+def logout():
+    print("Logout")
+
 def main():
     print("Hello, World!")
 
 if __name__ == "__main__":
     login()
     main()
+    logout()
